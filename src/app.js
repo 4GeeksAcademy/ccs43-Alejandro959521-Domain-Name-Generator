100d8060a28290fbaf7bb0b48ecb63dc6e9a339e
Miguel Ojeda <113554869+Alejandro959521@users.noreply.github.com>
2023-06-12 16:52:49 +0000

    cambio de la variable del for

--- src/app.js
+++ src/app.js
@@ -3,11 +3,11 @@ var adj = ["great", "big"];
 var noun = ["jogger", "racoon"];
 var link = [".com", ".net", ".es"];
 
-for (let x of pronoun) {
-  for (let y of adj) {
-    for (let z of noun) {
-      for (let a of link) {
-        console.log(x + y + z + a);
+for (let frase_1 of pronoun) {
+  for (let frase_2 of adj) {
+    for (let frase_3 of noun) {
+      for (let frase_4 of link) {
+        console.log(frase_1 + frase_2 + frase_3 + frase_4);
       }
     }
   }
